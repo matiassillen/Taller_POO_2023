ae2a8a658b6034a865f13e6cee32816a4e5ab276
matias <matias.sillen@gmail.com>
2023-09-14 01:07:34 -0300

    Agregamos el proyecto

---
+++ src/main/java/Taller_2023.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Matías Sillen
+ */
+public class Taller_2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
