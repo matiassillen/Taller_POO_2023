80d0c70b046ffe3ed23f0747532deff1f4272638
Yair N Chavez Coradini <yairchavezc@gmail.com>
2023-09-25 20:01:42 -0300

    Triage

--- src/main/java/Model/Triage.java
+++ src/main/java/Model/Triage.java
@@ -1,276 +1,367 @@
 package Model;
 
-
 import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import com.example.Variable;
+import javax.swing.plaf.synth.ColorType;
+import Estado;
+import TipoColor;
+import TDosEstados;
+import TEdad;
+import Consulta;
+import Medico;
+import LicEnEnfermeria;
+
 
 /**
  * 
  */
 public class Triage {
 
-    private String MotivoCambio;
-    private TipoColor ColorOriginal;
-    private TipoColor ColorFinal;
-    private Estado Respiración;
-    private TDosEstados Pulso;
-    private Estado EstadoMental;
-    private TDosEstados Conciencia;
-    private Boolean DificultadRespiratoria;
-    private Boolean LesionesGraves;
-    private Boolean LesionesLeves;
-    private TEdad Edad;
-    private Estado Fiebre;
-    private Estado Vómitos;
-    private Estado DolorAbdominal;
-    private Boolean SignosShock;
-    private Estado Sangrado;
-    
+    private String motivoCambio;
+    private TipoColor colorOriginal;
+    private TipoColor colorFinal;
+    private Estado respiración;
+    private TDosEstados pulso;
+    private Estado estadoMental;
+    private TDosEstados conciencia;
+    private Boolean dificultadRespiratoria;
+    private Boolean lesionesGraves;
+    private Boolean lesionesLeves;
+    private TEdad edad;
+    private Estado fiebre;
+    private Estado vómitos;
+    private Estado dolorAbdominal;
+    private Boolean signosShock;
+    private Estado sangrado;
+    private Consulta consulta;
+    private Medico realizoTriage;
+    private LicEnEnfermeria enfermero;
+ 
+
     public Triage() {
     }
+    
+    public Triage(String motivoCambio, TipoColor colorOriginal, TipoColor colorFinal, Estado respiración,
+            TDosEstados pulso, Estado estadoMental, TDosEstados conciencia, Boolean dificultadRespiratoria,
+            Boolean lesionesGraves, Boolean lesionesLeves, TEdad edad, Estado fiebre, Estado vómitos,
+            Estado dolorAbdominal, Boolean signosShock, Estado sangrado) {
+        this.MotivoCambio = motivoCambio;
+        this.ColorOriginal = colorOriginal;
+        this.ColorFinal = colorFinal;
+        this.Respiración = respiración;
+        this.Pulso = pulso;
+        this.EstadoMental = estadoMental;
+        this.Conciencia = conciencia;
+        this.DificultadRespiratoria = dificultadRespiratoria;
+        this.LesionesGraves = lesionesGraves;
+        this.LesionesLeves = lesionesLeves;
+        this.Edad = edad;
+        this.Fiebre = fiebre;
+        this.Vómitos = vómitos;
+        this.DolorAbdominal = dolorAbdominal;
+        this.SignosShock = signosShock;
+        this.Sangrado = sangrado;
+    }
 
-    public String ObtenerColor() {
-        // TODO implement here
-        return "";
+    public Triage(String motivoCambio, TipoColor colorOriginal, TipoColor colorFinal, Estado respiración,
+            TDosEstados pulso, Estado estadoMental, TDosEstados conciencia, Boolean dificultadRespiratoria,
+            Boolean lesionesGraves, Boolean lesionesLeves, TEdad edad, Estado fiebre, Estado vómitos,
+            Estado dolorAbdominal, Boolean signosShock, Estado sangrado, Consulta consulta, Medico medico, LicEnEnfermeria Lic) {
+        this.MotivoCambio = motivoCambio;
+        this.ColorOriginal = colorOriginal;
+        this.ColorFinal = colorFinal;
+        this.Respiración = respiración;
+        this.Pulso = pulso;
+        this.EstadoMental = estadoMental;
+        this.Conciencia = conciencia;
+        this.DificultadRespiratoria = dificultadRespiratoria;
+        this.LesionesGraves = lesionesGraves;
+        this.LesionesLeves = lesionesLeves;
+        this.Edad = edad;
+        this.Fiebre = fiebre;
+        this.Vómitos = vómitos;
+        this.DolorAbdominal = dolorAbdominal;
+        this.SignosShock = signosShock;
+        this.Sangrado = sangrado;
+        this.consulta = consulta;
+        this.medico = medico;
+        this.LicEnEnfermeria = lic;
+        comparar();
     }
 
     /**
      * @return
      */
     public String getMotivoCambio() {
-        // TODO implement here
-        return "";
+        return MotivoCambio;
     }
 
     /**
      * @param value
      */
     public void setMotivoCambio(String value) {
-        // TODO implement here
+        this.MotivoCambio = value
     }
 
     /**
      * @return
      */
-    public String getColorOriginal() {
-        // TODO implement here
-        return "";
+    public TipoColor getColorOriginal() {
+        return ColorOriginal;
     }
 
     /**
      * @param value
      */
     public void setColorOriginal(String value) {
-        // TODO implement here
+        this.ColorOriginal = value;
     }
 
     /**
      * @return
      */
-    public String getColorFinal() {
-        // TODO implement here
-        return "";
+    public TipoColor getColorFinal() {
+        return ColorFinal;
     }
 
     /**
      * @param value
      */
     public void setColorFinal(String value) {
-        // TODO implement here
+        this.ColorFinal = value;
     }
 
     /**
      * @return
      */
     public Estado getRespiración() {
-        // TODO implement here
-        return null;
+        return Respiración;
     }
 
     /**
      * @param value
      */
     public void setRespiración(Estado value) {
-        // TODO implement here
+        this.Respiración = value;
     }
 
     /**
      * @return
      */
-    public Boolean getPulso() {
-        // TODO implement here
-        return null;
+    public TDosEstados getPulso() {
+        return Pulso;
     }
 
     /**
      * @param value
      */
     public void setPulso(Boolean value) {
-        // TODO implement here
+        this.Pulso = value;
     }
 
     /**
      * @return
      */
     public Estado getEstadoMental() {
-        // TODO implement here
-        return null;
+        return EstadoMental;
     }
 
     /**
      * @param value
      */
     public void setEstadoMental(Estado value) {
-        // TODO implement here
+        this.EstadoMental = value;
     }
 
     /**
      * @return
      */
-    public Boolean getConciencia() {
-        // TODO implement here
-        return null;
+    public TDosEstados getConciencia() {
+        return Conciencia;
     }
 
     /**
      * @param value
      */
     public void setConciencia(Boolean value) {
-        // TODO implement here
+        this.Conciencia = value;
     }
 
     /**
      * @return
      */
     public Boolean getDificultadRespiratoria() {
-        // TODO implement here
-        return null;
+        return DificultadRespiratoria;
     }
 
     /**
      * @param value
      */
     public void setDificultadRespiratoria(Boolean value) {
-        // TODO implement here
+        this.DificultadRespiratoria = value;
     }
 
     /**
      * @return
      */
     public Boolean getLesionesGraves() {
-        // TODO implement here
-        return null;
+        return LesionesGraves;
     }
 
     /**
      * @param value
      */
     public void setLesionesGraves(Boolean value) {
-        // TODO implement here
+        this.LesionesGraves = value;
     }
 
     /**
      * @return
      */
     public Boolean getLesionesLeves() {
-        // TODO implement here
-        return null;
+        return LesionesLeves;
     }
 
     /**
      * @param value
      */
     public void setLesionesLeves(Boolean value) {
-        // TODO implement here
+        this.lesionesLeves = value;
     }
 
     /**
      * @return
      */
-    public Boolean getEdad() {
-        // TODO implement here
-        return null;
+    public TEdad getEdad() {
+        return Edad;
     }
 
     /**
      * @param value
      */
-    public void setEdad(Boolean value) {
-        // TODO implement here
+    public void setEdad(TEdad value) {
+        this.Edad = value;
     }
 
     /**
      * @return
      */
     public Estado getFiebre() {
-        // TODO implement here
-        return null;
+        return Fiebre;
     }
 
     /**
      * @param value
      */
     public void setFiebre(Estado value) {
-        // TODO implement here
+        this.Fiebre = value;
     }
 
     /**
      * @return
      */
     public Estado getVómitos() {
-        // TODO implement here
-        return null;
+        return Vómitos;
     }
 
     /**
      * @param value
      */
     public void setVómitos(Estado value) {
-        // TODO implement here
+        this.Vómitos = value;
     }
 
     /**
      * @return
      */
     public Estado getDolorAbdominal() {
-        // TODO implement here
-        return null;
+        return DolorAbdominal;
     }
 
     /**
      * @param value
      */
     public void setDolorAbdominal(Estado value) {
-        // TODO implement here
+        this.DolorAbdominal = value;
     }
 
     /**
      * @return
      */
     public Boolean getSignosShock() {
-        // TODO implement here
-        return null;
+        return SignosShock;
     }
 
     /**
      * @param value
      */
     public void setSignosShock(Boolean value) {
-        // TODO implement here
+        this.SignosShock = value;
     }
 
     /**
      * @return
      */
     public Estado getSangrado() {
-        // TODO implement here
-        return null;
+        return Sangrado;
     }
 
     /**
      * @param value
      */
     public void setSangrado(Estado value) {
-        // TODO implement here
+        this.Sangrado = value;
+    }
+
+    /**
+    * @void
+    */
+    public void obtenerPuntos() {
+        int puntos = 0;
+        puntos += this.respiración.getValorNumerico();
+        puntos += this.dolorAbdominal.getValorNumerico();
+        puntos += this.sangrado.getValorNumerico();
+        puntos += this.estadoMental.getValorNumerico();
+        puntos += this.signosShock.getValorNumerico();
+        puntos += this.edad.getValorNumerico();
+        puntos += this.dificultadRespiratoria.getValorNumerico();
+        puntos += this.pulso.getValorNumerico();
+        puntos += this.conciencia.getValorNumerico();
+        puntos += this.lesionesGraves.getValorNumerico();
+        puntos += this.lesionesLeves.getValorNumerico();
+        puntos += this.fiebre.getValorNumerico();
+        puntos += this.vómitos.getValorNumerico();
+        puntos += variablee.getValorNumerico();
+        this.setColorOriginal(obtenerColor(puntos));
+    }
+
+    /**
+    * @return
+    */
+    public TipoColor obtenerColor(int puntos) {
+        TipoColor color;
+        switch (puntos) {
+            case puntos>=15:
+                color = TipoColor.ROJO;
+                break;
+            case puntos>=10&&puntos<=14:
+                color = TipoColor.NARANJA;
+                break;
+            case puntos>=5&&puntos<=9:
+                color = TipoColor.AMARILLO;
+                break;
+            case puntos>=0&&puntos<=4:
+                color = TipoColor.VERDE;
+                break;
+            default:
+                color = TipoColor.AZUL;
+                break;
+            //Azul (Condiciones que pueden ser tratadas en un entorno no hospitalario):
+            //Dependerá del protocolo específico de la institución y las circunstancias.
+        }
+        return color;
     }
 
     /**
@@ -278,18 +369,48 @@ public class Triage {
      * @param fecha2 
      * @return
      */
-    public Integer CantidadDeTriageYColor(String fecha1, String fecha2) {
-        // TODO implement here
-        return null;
+    public static Map<TipoColor, Integer> triageFiltrarColorYFecha(String fecha1, String fecha2) {
+        Map<String, Integer> diccionario = new HashMap<>();
+        for (Codigo Para Recorer Arbol de Tiage) {
+            if (Clase.fecha<fecha1&&Clase.fecha<fecha2) {
+                tipoColor clave = Triage.ColorFinal;
+                if(diccionario.containsKey(clave)){
+                    diccionario.merge(clave, 1, Integer::sum);
+                }else{
+                    diccionario.put(clave, 1);
+                }
+            }            
+        }
+        return diccionario;
     }
 
     /**
      * @param FuncAdministrativo 
      * @return
      */
-    public Integer TriagesCambiados(FuncAdministrativo funcAdministrativo) {
-        // TODO implement here
-        return null;
+    public Integer TriagesCambiados(FuncSalud funcionario) {
+        int sum = 0;
+        int rango = len(funcionario.traigeHechos);
+        for (i= 0; i<rango; i++) {
+            Traige triageN = funcionario.traigeHechos[i];
+            if (triageN.colorFinal=!null) {
+                sum += 1;
+            }
+        }
+        return sum;
     }
-
+    
+    public void comparar(FuncionarioGeneral funcionario){
+        switch (funcionario.type) {
+            // Revisarrr;
+            case LicEnEnfermeria:
+                enfermero = funcionario;
+                medico = null;
+                break;
+            default:
+                medico = funcionario;
+                enfermero = null;
+                break;
+        }
+    }
 }
