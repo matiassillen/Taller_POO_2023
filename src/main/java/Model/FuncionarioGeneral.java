43f677f7f5d1a3029eb3f08a9376c38c0dc00756
agusfcb <agustinfbc@gmail.com>
2023-09-27 07:29:40 -0300

    "Agregado documentacion Sintomas y codigo FuncionarioGeneral"

--- src/main/java/Model/FuncionarioGeneral.java
+++ src/main/java/Model/FuncionarioGeneral.java
@@ -8,77 +8,99 @@ import java.util.*;
  */
 public class FuncionarioGeneral extends Persona {
 
+    private static int idTotales = 0;
+    private String nomUsuario;
+    private String passw;
+    private int idPersonal;
+    
+    //A debatir, si asignar rangos de id con privilegios segun rol o generar id globales
+    
     /**
-     * Default constructor
+     * Constructor de la clase Funcionario General
+     * @param nomUsuario Nombre de usuario para el sistema
+     * @param passw contraseña para la cuenta personal
+     * @param nombre Nombre real de la persona
+     * @param apellido  Apellido real de la persona
+     * @param fechaDeNac Fecha de nacimiento de la persona
+     * @param domicilio Domicilio de la persona
+     * @param dni numero de DNI de la persona
+     * @param telefonoFijo Numero de telefono fijo de la persona
+     * @param telefonoCel Numero de telefono movil de la persona
+     * @param correoE Correo electronico de la persona
+     * @param estadoCivil Estado civil de la persona
      */
-    public FuncionarioGeneral() {
+    
+    public FuncionarioGeneral(String nomUsuario, String passw, String nombre, String apellido, String fechaDeNac, String domicilio, int dni, int telefonoFijo, int telefonoCel, String correoE, String estadoCivil) {
+        super(nombre, apellido, fechaDeNac, domicilio, dni, telefonoFijo, telefonoCel, correoE, estadoCivil);
+        this.nomUsuario = nomUsuario;
+        this.passw = passw;
+        this.idPersonal = FuncionarioGeneral.generarId();
     }
 
     /**
-     * 
+     * Metodo que genera una ID para el usuario
+     * Accede y modifica la variable static idTotales de la clase Funcionario General
+     * @return devuelve un entero con el valor de la variable luego de su modificacion
      */
-    private String NomUsuario;
-
-    /**
-     * 
-     */
-    private String Passw;
-
-    /**
-     * 
-     */
-    private int Id;
-
-    /**
-     * 
-     */
+    private static int generarId(){
+        FuncionarioGeneral.idTotales += 1;
+        return FuncionarioGeneral.idTotales;
+    }
+            
     public void verifLogin() {
         // TODO implement here
     }
 
     /**
-     * @return
+     * @return Nombre de usuario
      */
     public String getNomUsuario() {
         // TODO implement here
-        return "";
+        return this.nomUsuario;
     }
 
     /**
-     * @param value
+     * @param value es el nuevo nombre de usuario
      */
     public void setNomUsuario(String value) {
-        // TODO implement here
+        this.nomUsuario = value;
     }
 
     /**
-     * @return
-     */
+    *@return retorna la contraseña
+    */
     public String getPassw() {
-        // TODO implement here
-        return "";
+        return passw;
+    }
+    
+    /**
+    *@param passw es la nueva contraseña
+    */
+    public void setPassw(String passw) {
+        this.passw = passw;
     }
 
     /**
-     * @param value
+     * @return retorna el id del propio usuario
      */
-    public void setPassw(String value) {
-        // TODO implement here
+    public String getIdPersonal() {
+        return String.format("%d",idPersonal);
     }
 
     /**
-     * @return
+     * Metodo para que un usuario con privilegios pueda ver la id de otro usuario
+     * @param otroFuncionario funcionario a consultar la Id
+     * @return retorna el valor de id del funcionario pasado por parametro
      */
-    public int getId() {
-        // TODO implement here
-        return 0;
+    public String getIdAdministrador(FuncionarioGeneral otroFuncionario){
+        if (this instanceof AdministradorDeSistema){
+            FuncionarioGeneral varTemp2 = otroFuncionario;
+            return String.format("%d", varTemp2.idPersonal);
+        }
+        else {
+            return "Error, usuario sin privilegios necesarios";
+            }
+        }
     }
-
-    /**
-     * @param value
-     */
-    public void setId(int value) {
-        // TODO implement here
-    }
-
-}
+    
+    // El siguiente metodo debe verificar que un funcionario tenga privilegios
