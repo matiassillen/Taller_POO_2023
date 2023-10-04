8fe2ca6d31e20800a5a16bae1198b34ebdce5c71
Facundo <fac.aguero94@gmail.com>
2023-10-04 00:29:21 -0300

    pantalla mas grande interfaz grafica recepcion

--- src/main/java/VentanasGUI/Recepcion.java
+++ src/main/java/VentanasGUI/Recepcion.java
@@ -101,7 +101,7 @@ public class Recepcion extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE)
         );
 
         pack();
