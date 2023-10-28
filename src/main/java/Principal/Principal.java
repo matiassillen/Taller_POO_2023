e2902ef758ad4564228b778ece02707f6279ee53
Matías Sillen Ríos <matias.sillen@gmail.com>
2023-10-28 05:20:16 -0300

    Modificaciones en interfaz Administrador

--- src/main/java/Principal/Principal.java
+++ src/main/java/Principal/Principal.java
@@ -1,15 +1,20 @@
 
 package Principal;
 
-import VentanasGUI.GuiLogin;
+import Model.Controladora;
+import VentanasGUI.Administrador;
 
 public class Principal {
-
+    
     public static void main(String[] args) {
+        Controladora control = new Controladora();
+//        GuiLogin pantalla= new GuiLogin();
+//        pantalla.setVisible(true);
+//        pantalla.setLocationRelativeTo(null);
+
+        Administrador pantalla = new Administrador(control);
         
-        GuiLogin pantalla= new GuiLogin();
-        pantalla.setVisible(true);
-        pantalla.setLocationRelativeTo(null);
+        
     }
     
 }
